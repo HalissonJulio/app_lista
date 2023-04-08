84f9c1118dfd0d4ba359bf5c54117b76d7340b4f
HalissonJulio <halissonjulio@gmail.com>
2023-04-07 21:47:41 -0300

    Corrigido os erros (eu espero).

---
+++ app/src/main/java/pereira/lopes/julio/galeria/activity/model/MyItem.java
@@ -0,0 +1,8 @@
+package pereira.lopes.julio.galeria.activity.model;
+
+import android.net.Uri;
+public class MyItem {
+    public Uri photo; //Guarda o link da img
+    public String title; //Guarda o titulo
+    public String description; //Guarda a descriçao
+}
